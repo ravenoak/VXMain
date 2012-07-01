d0f212c46de7ff2a917b03fc73d0efe540357512
Caitlyn O'Hanna <ravenoak@gmail.com>
2012-06-30 17:08:52 -0700

    Using tgext.admin as main admin controller. Will get rid of create/update/delete in page controller and others.

--- VXMain/controllers/admin.py
+++ VXMain/controllers/admin.py
@@ -1,96 +1,33 @@
 '''
-Created on Jul 25, 2011
+Created on Jun 28, 2012
 
 @author: ravenoak
 '''
 
-from VXMain import model
-from VXMain.model import DBSession
-from tg.controllers import TGController
-from tg.decorators import with_trailing_slash, override_template, expose
-from tg import config as tg_config
-from tgext.admin.config import AdminConfig
-from tgext.admin.tgadminconfig import TGAdminConfig
-from tgext.crud import CrudRestController
-from repoze.what.predicates import in_group
 
-engine = 'genshi'
-try:
-    import chameleon.genshi
-    import pylons.config
-    if 'renderers' in pylons.config and 'chameleon_genshi' in pylons.config['renderers']:
-        engine = 'chameleon_genshi'
-except ImportError:
-    pass
-
-class AdminController(TGController):
-    """
-    A basic controller that handles User Groups and Permissions for a TG application.
-    """
-    allow_only = in_group('administrators')
-
-    def __init__(self, models, session, config_type=None, translations=None):
-        super(AdminController, self).__init__()
-        if translations is None:
-            translations = {}
-        if config_type is None:
-            config = AdminConfig(models, translations)
-        else:
-            config = config_type(models, translations)
-
-        if config.allow_only:
-            self.allow_only = config.allow_only
-
-        self.config = config
-        self.session = session
-
-        default_renderer = getattr(tg_config,'default_renderer', 'genshi')
-        
-        self.default_index_template = ':'.join((default_renderer, self.index.decoration.engines.get('text/html')[1]))
-        if self.config.default_index_template:
-            self.default_index_template = self.config.default_index_template
-
-    @with_trailing_slash
-    @expose('tgext.admin.templates.index')
-    def index(self):
-        #overrides the template for this method
-        original_index_template = self.index.decoration.engines['text/html']
-        new_engine = self.default_index_template.split(':')
-        new_engine.extend(original_index_template[2:])
-        self.index.decoration.engines['text/html'] = new_engine
-        return dict(models=[model.__name__ for model in self.config.models.values()])
-
-    def _make_controller(self, config, session):
-        m = config.model
-        Controller = config.defaultCrudRestController
-        class ModelController(Controller):
-            model        = m
-            table        = config.table_type(session)
-            table_filler = config.table_filler_type(session)
-            new_form     = config.new_form_type(session)
-            new_filler   = config.new_filler_type(session)
-            edit_form    = config.edit_form_type(session)
-            edit_filler  = config.edit_filler_type(session)
-            allow_only   = config.allow_only
-        menu_items = None
-        if self.config.include_left_menu:
-            menu_items = self.config.models
-        return ModelController(session, menu_items)
-
-    @expose()
-    def _lookup(self, model_name, *args):
-        model_name = model_name[:-1]
-        try:
-            model = self.config.models[model_name]
-        except KeyError:
-            raise HTTPNotFound().exception
-        config = self.config.lookup_controller_config(model_name)
-        controller = self._make_controller(config, self.session)
-        return controller, args
-
-    @expose()
-    def lookup(self, model_name, *args):
-        return self._lookup(model_name, *args)
+#from VXMain import model
+#from VXMain.model import DBSession
+from VXMain.model.auth import User
+from repoze.what.predicates import has_permission
+from tgext.admin import AdminController, AdminConfig
+from tgext.admin.config import CrudRestControllerConfig
+from sprox.tablebase import TableBase
+from sprox.fillerbase import TableFiller
+#from sprox.dojo.tablebase import DojoTableBase as TableBase
+#from sprox.dojo.fillerbase import DojoTableFiller as TableFiller
 
 
-adminController = AdminController(model, DBSession, config_type = TGAdminConfig)
+class VXAdminConfig(AdminConfig):
+    class user(CrudRestControllerConfig):
+        class table_type(TableBase):
+                __entity__ = User
+                __limit_fields__ = ['display_name', 'email_address']
+                __url__ = '../user.json' #this just tidies up the URL a bit
+
+        class table_filler_type(TableFiller):
+                __entity__ = User
+                __limit_fields__ = ['display_name', 'email_address', 'user_name']
+
+
+class VXAdminController(AdminController):
+    allow_only = has_permission('administrators')
